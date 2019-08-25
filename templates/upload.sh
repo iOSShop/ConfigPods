31e94cf7defbe810ee7db69e5d41e99ea6645ce0
cctomato <cctomato@outlook.com>
2019-08-25 19:50:18 +0800

    Update upload.sh

--- templates/upload.sh
+++ templates/upload.sh
@@ -1,1 +1,1 @@
-pod repo push XLPTPPod __ProjectName__.podspec --verbose --allow-warnings --use-libraries --sources='https://github.com/iOSShop/ModularizationPod.git,https://github.com/CocoaPods/Specs.git'
+pod repo push ModularizationPod __ProjectName__.podspec --verbose --allow-warnings --use-libraries --sources='https://github.com/iOSShop/ModularizationPod.git,https://github.com/CocoaPods/Specs.git'
